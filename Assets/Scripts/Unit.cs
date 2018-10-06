25f6146b089093721e435bb4774811a9ca709e36
Edwin Jones Holanda <edwinjonesholanda@gmail.com>
2018-10-06 17:48:32 -0300

    Created unit

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Unit
+{
+    public int Money { get; private set; }
+
+    public int Rent { get; private set; }
+
+    public int Cost { get; private set; }
+
+    public Unit(int startMoney)
+    {
+        Money = startMoney;
+    }
+
+    public virtual void BuyHouse()
+    {
+
+    }
+
+    public void RollDice()
+    {
+
+    }
+
+    public void AddMoney(int amount)
+    {
+        Money += amount;
+    }
+
+    public int PayRent(int amount)
+    {
+        Money -= amount;
+
+        //pay the full amount if has money
+        //otherwise pay the full money
+        //if the unit has less the 0, pay nothing
+        return Money >= 0 ? amount : (Money < 0 ? 0 : Money);
+    }
+}
